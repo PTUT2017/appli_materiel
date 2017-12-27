26021690ac81c97f25be8f1d054f6e9f04eea70d
Jujuherrg <32415525+Jujuherrg@users.noreply.github.com>
2017-12-27 15:35:01 +0100

    Add files via upload

--- calendrier.php
+++ calendrier.php
@@ -14,90 +14,158 @@ catch (PDOException $e)
 echo "<div class='displaywrap'>";
 echo "<div id='video'>Vidéo";
 
-$requete="SELECT id_materiel, id_etudiant, id_reserver, date_debut, date_retour, quantite_reserver FROM reserver";
+
+$requete="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='video'";
 $reponse=$id_connex->query($requete);
 		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
-				$nb_date_debut=explode("-", $ligne['date_debut']);
-				$nb_date_debut=$nb_date_debut[0].$nb_date_debut[1].$nb_date_debut[2];
 
-				$nb_date_retour=explode("-", $ligne['date_retour']);
-				$nb_date_retour=$nb_date_retour[0].$nb_date_retour[1].$nb_date_retour[2];
+				$requete2=$requete="SELECT id_materiel, id_etudiant, id_reserver, date_debut, date_retour, quantite_reserver FROM reserver";
+				$reponse2=$id_connex->query($requete2);
+				$qtotal=$ligne['quantite_total'];
 
-				$nb_date=explode("-", $caseclick);
-				$nb_date=$nb_date[0].$nb_date[1].$nb_date[2];
 
-				echo $nb_date." ".$nb_date_debut." ".$nb_date_retour."<br>";
+				while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
 
-				if($nb_date_debut<=$nb_date && $nb_date<=$nb_date_retour){
-								echo "compris<br>";
-								
-								// On affiche le matériel qui a été réserver
-								$requete2="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='video' AND id_materiel='".$ligne['id_materiel']."'";
-								$reponse2=$id_connex->query($requete2);
-								while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
-											$qtotal=$ligne2['quantite_total']-$ligne['quantite_reserver'];
-											echo "<br>".$qtotal." ".$ligne2['designation'];
-									}
+					$nb_date_debut=explode("-", $ligne2['date_debut']);
+					$nb_date_debut=$nb_date_debut[0].$nb_date_debut[1].$nb_date_debut[2];
 
-				}
-				else
-				{
-								// On affiche le matériel qui n'a pas été réserver
-								$requete2="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='video' AND id_materiel!='".$ligne['id_materiel']."'";
-								$reponse2=$id_connex->query($requete2);
-								while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
-											echo "<br>".$ligne2['quantite_total']." ".$ligne2['designation'];
-										}
-				}
+					$nb_date_retour=explode("-", $ligne2['date_retour']);
+					$nb_date_retour=$nb_date_retour[0].$nb_date_retour[1].$nb_date_retour[2];
+
+					$nb_date=explode("-", $caseclick);
+					$nb_date=$nb_date[0].$nb_date[1].$nb_date[2];
+
+
+					if($nb_date_debut<=$nb_date && $nb_date<=$nb_date_retour && $ligne['id_materiel']==$ligne2['id_materiel']){
+
+													$qtotal=$qtotal-$ligne2['quantite_reserver'];
+
+						}
+
+					}
+					echo "<br>".$qtotal." ".$ligne['designation'];
 
 		}
 
 echo "</div>";
+
+echo "<div id='video'>Audio";
+
+
+$requete="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='son'";
+$reponse=$id_connex->query($requete);
+		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
+
+				$requete2=$requete="SELECT id_materiel, id_etudiant, id_reserver, date_debut, date_retour, quantite_reserver FROM reserver";
+				$reponse2=$id_connex->query($requete2);
+				$qtotal=$ligne['quantite_total'];
+
+
+				while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
+
+					$nb_date_debut=explode("-", $ligne2['date_debut']);
+					$nb_date_debut=$nb_date_debut[0].$nb_date_debut[1].$nb_date_debut[2];
+
+					$nb_date_retour=explode("-", $ligne2['date_retour']);
+					$nb_date_retour=$nb_date_retour[0].$nb_date_retour[1].$nb_date_retour[2];
+
+					$nb_date=explode("-", $caseclick);
+					$nb_date=$nb_date[0].$nb_date[1].$nb_date[2];
+
+
+					if($nb_date_debut<=$nb_date && $nb_date<=$nb_date_retour && $ligne['id_materiel']==$ligne2['id_materiel']){
+
+													$qtotal=$qtotal-$ligne2['quantite_reserver'];
+
+						}
+
+					}
+					echo "<br>".$qtotal." ".$ligne['designation'];
+
+		}
+
+echo "</div>";
+
+echo "<div id='video'>Accessoires";
+
+
+$requete="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='accessoire'";
+$reponse=$id_connex->query($requete);
+		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
+
+				$requete2=$requete="SELECT id_materiel, id_etudiant, id_reserver, date_debut, date_retour, quantite_reserver FROM reserver";
+				$reponse2=$id_connex->query($requete2);
+				$qtotal=$ligne['quantite_total'];
+
+
+				while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
+
+					$nb_date_debut=explode("-", $ligne2['date_debut']);
+					$nb_date_debut=$nb_date_debut[0].$nb_date_debut[1].$nb_date_debut[2];
+
+					$nb_date_retour=explode("-", $ligne2['date_retour']);
+					$nb_date_retour=$nb_date_retour[0].$nb_date_retour[1].$nb_date_retour[2];
+
+					$nb_date=explode("-", $caseclick);
+					$nb_date=$nb_date[0].$nb_date[1].$nb_date[2];
+
+
+					if($nb_date_debut<=$nb_date && $nb_date<=$nb_date_retour && $ligne['id_materiel']==$ligne2['id_materiel']){
+
+													$qtotal=$qtotal-$ligne2['quantite_reserver'];
+
+						}
+
+					}
+					echo "<br>".$qtotal." ".$ligne['designation'];
+
+		}
+
+echo "</div>";
+
+echo "<div id='video'>Lumière";
+
+
+$requete="SELECT designation, quantite_total, id_materiel FROM materiel WHERE categorie='lumiere'";
+$reponse=$id_connex->query($requete);
+		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
+
+				$requete2=$requete="SELECT id_materiel, id_etudiant, id_reserver, date_debut, date_retour, quantite_reserver FROM reserver";
+				$reponse2=$id_connex->query($requete2);
+				$qtotal=$ligne['quantite_total'];
+
+
+				while ($ligne2 = $reponse2-> fetch(PDO::FETCH_ASSOC)){
+
+					$nb_date_debut=explode("-", $ligne2['date_debut']);
+					$nb_date_debut=$nb_date_debut[0].$nb_date_debut[1].$nb_date_debut[2];
+
+					$nb_date_retour=explode("-", $ligne2['date_retour']);
+					$nb_date_retour=$nb_date_retour[0].$nb_date_retour[1].$nb_date_retour[2];
+
+					$nb_date=explode("-", $caseclick);
+					$nb_date=$nb_date[0].$nb_date[1].$nb_date[2];
+
+
+					if($nb_date_debut<=$nb_date && $nb_date<=$nb_date_retour && $ligne['id_materiel']==$ligne2['id_materiel']){
+
+													$qtotal=$qtotal-$ligne2['quantite_reserver'];
+
+						}
+
+					}
+					echo "<br>".$qtotal." ".$ligne['designation'];
+
+		}
+
+echo "</div>";
+
+
 echo "</div>";
 
 
 
 
-
-
-echo "<div class='displaywrap'>";
-
-	$requete="SELECT designation, quantite_total FROM materiel WHERE categorie='video'";
-	$reponse=$id_connex->query($requete);
-	echo "<div id='video'>Vidéo";
-		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
-			echo "<br>".$ligne['quantite_total']." ".$ligne['designation'];
-				}
-	echo "</div>";
-
-
-	$requete="SELECT designation, quantite_total FROM materiel WHERE categorie='son'";
-	$reponse=$id_connex->query($requete);
-	echo "<div id='audio'>Audio";
-		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
-			echo "<br>".$ligne['quantite_total']." ".$ligne['designation'];
-				}
-	echo "</div>";
-
-
-	$requete="SELECT designation, quantite_total FROM materiel WHERE categorie='accessoire'";
-	$reponse=$id_connex->query($requete);
-	echo "<div id='accesoire'>Accesoires";
-		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
-			echo "<br>".$ligne['quantite_total']." ".$ligne['designation'];
-				}
-	echo "</div>";
-
-
-	$requete="SELECT designation, quantite_total FROM materiel WHERE categorie='eclairage'";
-	$reponse=$id_connex->query($requete);
-	echo "<div id='lumiere'>Lumiere";
-		while ($ligne = $reponse-> fetch(PDO::FETCH_ASSOC)){
-			echo "<br>".$ligne['quantite_total']." ".$ligne['designation'];
-				}
-	echo "</div>";
-
-echo "</div>";
 
 
 $reponse->closeCursor();
